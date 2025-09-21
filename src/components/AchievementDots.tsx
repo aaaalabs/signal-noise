3f5906281ca479b460f2aec2a1679519aed2c181
aaaalabs <aaa@libralab.ai>
2025-09-21 08:01:14 +0200

    claude: make Commitment Mode lock icon larger and more visible (12px, better styling)

--- src/components/AchievementDots.tsx
+++ src/components/AchievementDots.tsx
@@ -129,15 +129,14 @@ export default function AchievementDots({ data, earnedCount, onCommitmentModeCli
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
+            width="8"
+            height="8"
             viewBox="0 0 24 24"
             fill="none"
-            stroke="#444"
+            stroke="#666"
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            style={{ opacity: 0.3 }}
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z" />
@@ -157,8 +156,8 @@ export default function AchievementDots({ data, earnedCount, onCommitmentModeCli
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
+            width="8"
+            height="8"
             viewBox="0 0 24 24"
             fill="none"
             stroke="var(--signal)"
@@ -167,9 +166,9 @@ export default function AchievementDots({ data, earnedCount, onCommitmentModeCli
             strokeLinejoin="round"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M3 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z" />
-            <path d="M9 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" />
-            <path d="M13 11v-4a4 4 0 1 1 8 0v4" />
+            <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z" />
+            <path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" />
+            <path d="M8 11v-4a4 4 0 1 1 8 0v4" />
           </svg>
         </div>
       );
